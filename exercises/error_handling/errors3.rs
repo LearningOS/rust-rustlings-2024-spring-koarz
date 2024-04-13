5e9247ba89d67728a42ba723bd2eb2e32712276e
koarz <3577087577@qq.com>
2024-04-14 01:37:36 +0800

    15testpass

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,15 +7,13 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input).unwrap();
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -28,7 +26,9 @@ fn main() {
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
-
-    Ok(qty * cost_per_item + processing_fee)
+    let qty = item_quantity.parse::<i32>();
+    if let Err(_) = qty {
+        return qty;
+    }
+    Ok(qty.unwrap() * cost_per_item + processing_fee)
 }
